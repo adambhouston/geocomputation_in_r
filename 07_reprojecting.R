4f7afc4cafb80a81f37f4f0dc7fa92c32588333e
adambhouston <50245445+adambhouston@users.noreply.github.com>
2022-06-05 10:57:42 -0400

    actually done

--- 07_reprojecting.R
+++ 07_reprojecting.R
@@ -1,5 +1,5 @@
 # 7 reprojecting geographic data----
-pacman::p_load(sf, terra, dplyr, spData, spDataLarge, lwgeom)
+pacman::p_load(sf, terra, dplyr, spData, spDataLarge, lwgeom, tmap)
 
 # * 7.1 introduction----
 # geographic crs has units in degrees long and lat
@@ -52,3 +52,85 @@ sf::sf_use_s2(TRUE)
 # in situations where curved boundaries are used, best to transform to a proj CRS
 # 
 # * 7.5 when to reproject?----
+# transforming to a proj crs is essential if doing geometric operations
+# leaflet package requires geographic CRS
+# many web mapps require epsg 4326, wgs 84
+# but planar geometry operations like buffering may require a projected crs
+#
+# * 7.6 which crs to use?----
+# wgs 84/4326 is the most widely used geographic CRS, also used by GPS
+# if using a public data set, probably best to stick with the CRS for that area
+# a good projected default is UTM universal transverse mercator
+# but best only to use if the area is relatively small and within 6 degrees of 
+# central meridian
+# crssuggest package takes a geographic crs and returns possible projected crs
+# 
+# * 7.7 reprojecting vector geometries----
+# use st_transform
+# 
+# * 7.8 reprojecting raster geometries----
+# raster reproj means creating a new object, with a different number of columns
+# and rows
+# attributes must be reestimated
+# essentially two operations: changing extent, then resample pixel values
+# best to avoid reproject rasters and reproject the vectors instead
+# raster reprojection (warping) is done with project()
+# raster transofmration can be done with stars
+# looking at NLCD for utah, categorical data
+cat_raster = rast(system.file("raster/nlcd.tif", package = "spDataLarge"))
+cat(crs(cat_raster))
+unique(cat_raster)
+# project into wgs84
+cat_raster_wgs84 = project(cat_raster, "EPSG:4326", method = "near")
+# if transforming numeric raster, use bilinear or other method rather than
+# nearest neighbor
+# 
+# * 7.9 custom map projections----
+# transform zion into custom azimuthal equidistant crs
+zion = read_sf(system.file("vector/zion.gpkg", package = "spDataLarge"))
+# calculate centroid
+zion_centr = st_centroid(zion)
+zion_centr_wgs84 = st_transform(zion_centr, "EPSG:4326")
+st_as_text(st_geometry(zion_centr_wgs84))
+# now modify the wkt definition of the AEQD CRS using this centroid point
+# changing the central meridian and the latitude of origin
+my_wkt = 'PROJCS["Custom_AEQD",
+ GEOGCS["GCS_WGS_1984",
+  DATUM["WGS_1984",
+   SPHEROID["WGS_1984",6378137.0,298.257223563]],
+  PRIMEM["Greenwich",0.0],
+  UNIT["Degree",0.0174532925199433]],
+ PROJECTION["Azimuthal_Equidistant"],
+ PARAMETER["Central_Meridian",-113.0263],
+ PARAMETER["Latitude_Of_Origin",37.29818],
+ UNIT["Meter",1.0]]'
+zion_aeqd = st_transform(zion, my_wkt)
+
+# mollweide projection is good for world mapping while preserving area
+world_mollweide = st_transform(world, crs = "+proj=moll")
+world_mollweide_gr = st_graticule(lat = c(-89.9, seq(-80, 80, 20), 89.9)) %>%
+  lwgeom::st_transform_proj(crs = "+proj=moll")
+tm_shape(world_mollweide_gr) +
+  tm_lines(col = "gray") +
+  tm_shape(world_mollweide) +
+  tm_borders(col = "black") 
+
+# * 7.10 exercises
+# 1
+nz_wgs = st_transform(nz, crs = "EPSG:4326") 
+st_crs(nz_wgs)
+
+# 2
+world_merc = st_transform(world, crs = "+proj=tmerc")
+st_crs(world_merc)
+plot(st_geometry(world))
+plot(st_geometry(world_merc))
+world_wgs = st_transform(world_merc, crs = "EPSG:4326")
+plot(st_geometry(world_wgs))
+
+# 3 
+
+# 4
+transform = project(cat_raster, "EPSG:4326", method = "bilinear")
+# turns a categorical into a continuous raster
+# 
